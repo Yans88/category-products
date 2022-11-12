4a5d0406ef5a3e0278f22295486cbbdc15b02dc9
Yansen <hanssn88@gmail.com>
2022-11-12 10:02:50 +0700

    update readme

--- src/main/java/com/yansen/controllers/WelcomeController.java
+++ src/main/java/com/yansen/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.yansen;
+package com.yansen.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping
-    public String welcone(){
+    public String welcone() {
         return "Welcome to spring boot API - Test DayaLima";
     }
-    
+
 }
